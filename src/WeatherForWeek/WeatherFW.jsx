d2085d4231fc22aa41a1ee880ff62920e21dc713
Настя Скрицька <snastia.m@gmail.com>
2024-04-13 12:58:01 +0300

    weather for week

--- src/WeatherForWeek/WeatherFW.jsx
+++ src/WeatherForWeek/WeatherFW.jsx
@@ -1,28 +1,37 @@
 import { useEffect, useState } from "react";
+import { Container, Item, List } from "./WeatherFW.styled";
 
 export function WeatherFW () {
 
-const [weather, setWeather] = useState("")
+const [weather, setWeather] = useState([])
 
 useEffect(() => {
-  
-    fetch(`api.openweathermap.org/data/2.5/forecast/daily?lat=44.34&lon=10.99&cnt=7&appid=40207e285e43c5b8e49ba7f2599cdd4b`)
+    fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=44.34&lon=10.99&appid=40207e285e43c5b8e49ba7f2599cdd4b`)
     .then(res => res.json())
-    .then(weather => {console.log(weather)})
+    .then(weather => weather.daily)
+    .then(daily => setWeather(daily))
 }, [])
 
+function getDate(millisecs){
+   return Math.floor(millisecs / (1000 * 60 * 60 * 24))
+}
+
+function getCelsius(grade){
+    return Math.floor(grade - 273,16)
+}
+
     return(
-        <div>
+        <Container>
             <p>8-day forecast</p>
-            <ul>
-           {/* {li.map(() => (
-            <li>
-                <p>{date}</p>
-                <p>{grade}</p>
-                <p>{weather}</p> */}
-            {/* </li> */}
-         {/* ))} */}
-    </ul>
-</div> 
+            <List>              
+           {weather.length > 0 && weather.map((date) => (
+            <Item key={date.dt}>
+                <p>{new Date(date.dt).toDateString()}</p>
+                <p>{getCelsius(date.temp.min)} / {getCelsius(date.temp.max)}</p>
+                <p>{date.summary}</p> 
+            </Item>
+          ))}
+    </List>
+</Container> 
 )
 }
--- src/WeatherForWeek/WeatherFW.styled.jsx
+++ src/WeatherForWeek/WeatherFW.styled.jsx
@@ -1,1 +1,72 @@
 import styled from "styled-components"
+
+export const Container = styled.div`
+    background-color: #E8E8E8;
+    width: 1140px;
+    height: 554px;
+    top: 2513px;
+    gap: 0px;
+    border-radius: 20px;
+    margin-left: 160px;
+`
+
+export const List = styled.ul`
+    list-style: none;
+    display: flex;
+`
+export const Item = styled.li`
+    margin: 15px;
+`
+
+// fontSizes: {
+// xs: '12px',
+// s: '14px',
+// m: '16px',
+// l: '32px',
+// xl: '64px',
+// },
+
+// export const theme = {
+// colors: {
+//     black: '#000',
+//     white: '#fff',
+//     text: '#2a2a2a',
+//     background: '#fff',
+//     primary: '#07c',
+//     secondary: '#05a',
+//     accent: '#609',
+//     muted: '#f6f6f6',
+//     },
+//     space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+//     fonts: {
+//     body: 'system-ui, sans-serif',
+//     heading: 'system-ui, sans-serif',
+//     monospace: 'Menlo, monospace',
+//     },
+//     fontSizes: {
+//     xs: '12px',
+//     s: '14px',
+//     m: '16px',
+//     l: '32px',
+//     xl: '64px',
+//     },
+//     fontWeights: {
+//     normal: 400,
+//     bold: 700,
+//     },
+//     lineHeights: {
+//     body: 1.5,
+//     heading: 1.125,
+//     },
+//     borders: {
+//     none: 'none',
+//     normal: '1px solid',
+//     },
+//     radii: {
+//     none: '0',
+//     sm: '4px',
+//     md: '8px',
+//     lg: '16px',
+//     round: '50%',
+//     },
+//     };
