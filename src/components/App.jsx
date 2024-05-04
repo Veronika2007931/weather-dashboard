8017d5d6ab4b604a4c9cb3eb7c0a9ce73ca2905a
Ivan <vandoslando@gmail.com>
2024-05-04 11:00:12 +0300

    hourly forecast

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,22 @@
-
-import { WeatherFW } from "components/WeatherForWeek/WeatherFW";
+import { useState } from "react";
 import { Header } from "./Header/Header"
+import { CityList } from "./CityCard/CityList";
 import { WeatherFW } from "components/WeatherForWeek/WeatherFW";
 import { Gallery } from "./TeamGalery/Gallery";
 import { Footer } from "./Footer/Footer"
 import {Container} from "../Container.styled"
+import { HourlyForecast } from "./HourlyForecast/HourlyForecast";
 
 
 
 export const App = () => {
+  const [forecastData, setForecastData] = useState(null)
+
   return (
     <Container>
       <Header/>
+      <CityList setForecastData={setForecastData}/>
+      <HourlyForecast forecastData={forecastData}/>
       <WeatherFW/>
       <Gallery/>
       <Footer/>
