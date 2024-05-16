da145f37dcbc1ca479448ca80c47afa8e678615c
Daniil <negativdaniil4@gmail.com>
2024-05-17 00:11:39 +0300

    slaider

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,13 @@ import { Header } from "./Header/Header"
 // import CityList from "./CityCard/CityList.styled"
 import { WeatherFW } from "components/WeatherForWeek/WeatherFW";
 // import { NewsSection } from "./News/News";
-import { Gallery } from "./TeamGalery/Gallery";
+// import { Gallery } from "./TeamGalery/Gallery";
 import { Footer } from "./Footer/Footer"
 import {Container} from "../Container.styled"
 // import { HourlyForecast } from "./HourlyForecast/HourlyForecast";
 import { Search } from './Search/Search'
 import { GlobalStyle } from "Global.styled";
+// import { Slaider } from "./Slaider/Slaider";
 
 
 
@@ -25,9 +26,10 @@ export const App = () => {
       {/* <CityList setForecastData={setForecastData}/> */}
       {/* <HourlyForecast forecastData={forecastData}/> */}
     <WeatherFW/>
-    <Gallery/>
+    {/* <Gallery/> */}
       {/* <NewsSection/> */}
     <GlobalStyle/>
+    {/* <Slaider/> */}
     <Footer/>
     </Container>
     
---
+++ src/components/Slaider/Slaider.jsx
@@ -0,0 +1,13 @@
+import Carousel from "./Slaider.styled"
+
+
+export function Slaider(){
+
+    return <Carousel>
+            <div className="item item-1">Item 1</div>
+            <div className="item item-2">Item 2</div>
+            <div className="item item-3">Item 3</div>
+        </Carousel>
+}
+
+export default Slaider;
