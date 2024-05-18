bca57f3d456241b79f44be01673af4ce5e64f6b3
fedorbor <112242236+fedorbor@users.noreply.github.com>
2024-05-18 10:56:40 +0300

    ok

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,29 @@
-
-import { useState } from "react";
-import { Header } from "./Header/Header"
-// import { CityList } from "./CityCard/CityList.styled";
-// import CityList from "./CityCard/CityList.styled"
+import React, { useState } from "react";
+import { Header } from "./Header/Header";
+import CityList from "./CityCard/CityList";
 import { WeatherFW } from "components/WeatherForWeek/WeatherFW";
-// import { NewsSection } from "./News/News";
 import { Gallery } from "./TeamGalery/Gallery";
-import { Footer } from "./Footer/Footer"
-import {Container} from "../Container.styled"
+import { Footer } from "./Footer/Footer";
+import { Container } from "../Container.styled";
 // import { HourlyForecast } from "./HourlyForecast/HourlyForecast";
-import { Search } from './Search/Search'
+import { Search } from "./Search/Search";
 import { GlobalStyle } from "Global.styled";
 
-
-
 export const App = () => {
-  const [forecastData, setForecastData] = useState(null)
+  const [forecastData, setForecastData] = useState(null);
 
   return (
-    <>
     <Container>
-    <Header/>
-    <Search/>
-      {/* <CityList setForecastData={setForecastData}/> */}
-      {/* <HourlyForecast forecastData={forecastData}/> */}
-    <WeatherFW/>
-    <Gallery/>
-      {/* <NewsSection/> */}
-    <GlobalStyle/>
-    <Footer/>
+      <Header />
+      <Search />
+      <CityList setForecastData={setForecastData} />
+      <HourlyForecast forecastData={forecastData} />
+      <WeatherFW />
+      <Gallery />
+      <GlobalStyle/>
+      <Footer />
     </Container>
-    
-    </>
   );
 };
 
+export default App;
